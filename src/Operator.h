75a03083896c936e6ed5afdf27a0206805e64738
kaosad <frasker@hotmail.com>
2014-08-13 14:50:40 +0800

    Still add some silly function headers for demo

--- src/Operator.h
+++ src/Operator.h
@@ -6,9 +6,7 @@
 #include "ErrorCode.h"
 
 void getInfixValues(int *left, int *right, Stack *dataStack);
-void rubbishForTesting(int *left, int *right, Stack *dataStack);
-void pushNewNumber(int result, Stack *dataStack);
-void wongTryToBeFunny();
+void thisIsSilly();
 void executeAdd(Stack *dataStack, Stack *operatorStack);
 void executeSub(Stack *dataStack, Stack *operatorStack);
 void executeMul(Stack *dataStack, Stack *operatorStack);
@@ -16,6 +14,9 @@ void executeModulo(Stack *dataStack, Stack *operatorStack);
 void executeDiv(Stack *dataStack, Stack *operatorStack);
 void executeOr(Stack *dataStack, Stack *operatorStack);
 void executeXor(Stack *dataStack, Stack *operatorStack);
+void rubbishForTesting(int *left, int *right, Stack *dataStack);
+void pushNewNumber(int result, Stack *dataStack);
+void wongTryToBeFunny();
 void executeAnd(Stack *dataStack, Stack *operatorStack);
 void executeBitAnd(Stack *dataStack, Stack *operatorStack);
 void executeBitOr(Stack *dataStack, Stack *operatorStack);
